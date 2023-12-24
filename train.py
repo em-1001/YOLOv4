57457ac4f864cca0a352d941e6bf6320cd394680
em-1001 <80628552+em-1001@users.noreply.github.com>
2023-12-24 18:35:54 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+# refernce : https://github.com/aladdinpersson/Machine-Learning-Collection/blob/master/ML/Pytorch/object_detection/YOLOv3/train.py
+
 import config
 import torch
 import torch.optim as optim
