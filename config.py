ec6092cd358fea5460421c5a775864e79f57efaf
em-1001 <80628552+em-1001@users.noreply.github.com>
2024-01-04 22:46:20 +0900

    Update config.py

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ NUM_WORKERS = 4
 BATCH_SIZE = 32
 IMAGE_SIZE = 416
 NUM_CLASSES = 20
-LEARNING_RATE = 1e-5
-WEIGHT_DECAY = 1e-4
+LEARNING_RATE = 0.001 # YOLOv3 -> 1e-5
+WEIGHT_DECAY = 0.0005 # YOLOv3 -> 1e-4
 NUM_EPOCHS = 100
 CONF_THRESHOLD = 0.05
 MAP_IOU_THRESH = 0.5
@@ -28,9 +28,19 @@ CHECKPOINT_FILE = "/content/drive/MyDrive/yolov3/checkpoint.pth.tar"
 IMG_DIR = DATASET + "/images/"
 LABEL_DIR = DATASET + "/labels/"
 
+# YOLOv3 ANCHORS
+''' 
 ANCHORS = [
     [(0.28, 0.22), (0.38, 0.48), (0.9, 0.78)],
     [(0.07, 0.15), (0.15, 0.11), (0.14, 0.29)],
+    [(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)],
+]  # Note these have been rescaled to be between [0, 1]
+'''
+
+# YOLOv4 ANCHORS
+ANCHORS = [
+    [(0.30, 0.23), (0.41, 0.52), (0.99, 0.87)],
+    [(0.07, 0.16), (0.16, 0.11), (0.15, 0.31)],
     [(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)],
 ]  # Note these have been rescaled to be between [0, 1]
 
