2fec7592337f486e93b141cfad5a43b4b634134b
em-1001 <80628552+em-1001@users.noreply.github.com>
2024-01-07 17:49:24 +0900

    Update v4loss.py

--- v4loss.py
+++ v4loss.py
@@ -12,16 +12,16 @@ class YoloLoss(nn.Module):
     
     def __init__(self):
         super().__init__()
-        self.mse = nn.MSELoss()
-        self.bce = nn.BCEWithLogitsLoss()
+        self.mse = nn.MSELoss(reduction='mean')
+        self.bce = nn.BCEWithLogitsLoss(reduction='mean')
         self.entropy = nn.CrossEntropyLoss()
         self.sigmoid = nn.Sigmoid()
 
         # Constants signifying how much to pay for each respective part of the loss
-        self.lambda_class = 1
+        self.lambda_class = 2
         self.lambda_noobj = 1 #0.5
-        self.lambda_obj = 1
-        self.lambda_box = 1 #2.5
+        self.lambda_obj = 2
+        self.lambda_box = 2 #2.5
 
     def forward(self, predictions, target, anchors, box_loss="MSE"):
         # Check where obj and noobj (we ignore if target == -1)
@@ -32,7 +32,7 @@ class YoloLoss(nn.Module):
         # No Object Loss
         no_object_loss = self.bce(
             (predictions[..., 0:1][noobj]), (target[..., 0:1][noobj]),
-        )
+        ) 
 
 
         # Object Loss
@@ -41,7 +41,7 @@ class YoloLoss(nn.Module):
         ious = intersection_over_union(box_preds[obj], target[..., 1:5][obj]).detach()
         object_loss = self.bce(
           (predictions[..., 0:1][obj]), (ious * target[..., 0:1][obj])
-        )
+        ) 
 
 
         # Box Loss
@@ -50,7 +50,7 @@ class YoloLoss(nn.Module):
             target[..., 3:5] = torch.log(
                 (1e-16 + target[..., 3:5] / anchors)
             )  # width, height coordinates
-            box_loss = self.mse(predictions[..., 1:5][obj], target[..., 1:5][obj])
+            box_loss = self.mse(predictions[..., 1:5][obj], target[..., 1:5][obj]) 
 
         elif box_loss == "IoU" or box_loss == "GIoU" or box_loss == "DIoU" or box_loss == "CIoU":
             anchors = anchors.reshape(1, 3, 1, 1, 2) # 3(anchor) x 2(h, w), p_w * exp(t_w)를 연산하기 위해 reshape
@@ -62,18 +62,18 @@ class YoloLoss(nn.Module):
 
 
         # Class Loss
-        """
+        
         class_target = torch.zeros_like(predictions[..., 5:][obj])
         for i in range(class_target.shape[0]):
             class_target[i][int(target[..., 5][obj][i])] = 1.
 
         class_loss = self.bce(
             (predictions[..., 5:][obj]), (class_target)
-        )"""
-
+        ) 
+        """
         class_loss = self.entropy(
             (predictions[..., 5:][obj]), (target[..., 5][obj].long()),
-        )
+        )"""
 
         #print("__________________________________")
         #print(self.lambda_box * box_loss)
