830674455c5dbfff6a249cb996014c39d3585c2e
em-1001 <80628552+em-1001@users.noreply.github.com>
2024-01-11 23:54:25 +0900

    Update config.py

--- config.py
+++ config.py
@@ -29,21 +29,21 @@ IMG_DIR = DATASET + "/images/"
 LABEL_DIR = DATASET + "/labels/"
 
 # YOLOv3 ANCHORS
-''' 
+
 ANCHORS = [
     [(0.28, 0.22), (0.38, 0.48), (0.9, 0.78)],
     [(0.07, 0.15), (0.15, 0.11), (0.14, 0.29)],
     [(0.02, 0.03), (0.04, 0.07), (0.08, 0.06)],
 ]  # Note these have been rescaled to be between [0, 1]
-'''
 
+'''
 # YOLOv4 ANCHORS
 ANCHORS = [
     [(0.34, 0.26), (0.46, 0.58), (0.99, 0.95)],
     [(0.08, 0.18), (0.18, 0.13), (0.17, 0.35)],
     [(0.02, 0.03), (0.04, 0.08), (0.09, 0.06)],
 ]  # Note these have been rescaled to be between [0, 1]
-
+'''
 
 scale = 1.1
 train_transforms = A.Compose(
